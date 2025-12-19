2391f957452d04d519a1927fc3e11194aa85b0c0
teruteru-source <teruki1331@gmail.com>
2025-12-19 13:32:06 +0900

    Implement fraction class for arithmetic operations

--- calc_L_quotient.cpp
+++ calc_L_quotient.cpp
@@ -1,3 +1,5 @@
+//コードはC++で記述
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,9 +7,108 @@
 #include <cassert>
 #include <numeric>
 #include <iomanip>
-#include "fraction.hpp"
 
 std::vector<int> Parametrization_Degrees = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,-1,-1,1,1,-1,1,-1,1,1,1,-1,-1,1,-1,2,1,-1,-1,2};
+
+struct fraction{
+  int numerator,denominator;
+  
+  fraction() : numerator(0), denominator(1) {}
+  fraction(int x) : numerator(x), denominator(1) {}
+  fraction(int x,int y){
+    assert(y!=0);
+    int G = std::gcd(x,y);
+    x /= G,y /= G;
+    numerator=x,denominator=y;
+    if(denominator<0) numerator = -numerator, denominator = -denominator;
+  }//x/y
+  
+  fraction operator+(const fraction &o){
+    fraction res;
+    res.denominator = std::lcm(denominator,o.denominator);
+    res.numerator = res.denominator/denominator*numerator + res.denominator/o.denominator*o.numerator;
+    int G = std::gcd(res.denominator,res.numerator);
+    res.denominator /= G,res.numerator /= G;
+    return res;
+  }
+  fraction operator+(int x){return *this + fraction(x,1);}
+  
+  fraction operator-(const fraction &o){
+    fraction res(o.numerator,-o.denominator);
+    return *this + res;
+  }
+  fraction operator-(int x){return *this - fraction(x,1);}
+  fraction operator-(){return fraction(-numerator,denominator);}
+  
+  fraction operator*(const fraction &o){return fraction(numerator*o.numerator,denominator*o.denominator);}
+  fraction operator*(int x){return fraction(numerator*x,denominator);}
+  fraction operator/(const fraction &o){return fraction(numerator*o.denominator,denominator*o.numerator);}
+  fraction operator/(int x){return fraction(numerator,denominator*x);}
+  
+  fraction operator+=(const fraction &o){*this = *this + o; return *this;}
+  fraction operator+=(const int &o){*this = *this + o; return *this;}
+  fraction operator-=(const fraction &o){*this = *this - o; return *this;}
+  fraction operator-=(const int &o){*this = *this - o; return *this;}
+  fraction operator*=(const fraction &o){*this = *this * o; return *this;}
+  fraction operator*=(const int &o){*this = *this * o; return *this;}
+  fraction operator/=(const fraction &o){*this = *this / o; return *this;}
+  fraction operator/=(const int &o){*this = *this / o; return *this;}
+  
+  fraction operator%(int x){
+    numerator %= (x*denominator);
+    if(numerator<0) numerator += x*denominator;
+    return fraction(numerator,denominator);
+  }
+  
+  bool operator==(const fraction &o) const{return numerator*o.denominator == o.numerator*denominator;}
+  bool operator!=(const fraction &o) const{return !(*this == o);}
+  bool operator==(int x) const{return numerator == x*denominator;}
+  bool operator!=(int x) const{return !(*this==x);}
+  bool operator<(const fraction &o) const{return numerator*o.denominator < o.numerator*denominator;}
+  bool operator>(const fraction &o) const{return o < *this;}
+  bool operator<=(const fraction &o) const{return !(o < *this);}
+  bool operator>=(const fraction &o) const{return !(o > *this);}
+  
+  friend std::ostream& operator<<(std::ostream& os,const fraction &x){
+    os<<x.numerator<<'/'<<x.denominator;
+    return os;
+  }
+  
+  fraction inv(){
+    assert(numerator != 0);
+    return fraction(denominator,numerator);
+  }
+  
+  std::vector<int> continued_fraction(){//連分数展開
+    std::vector<int> res;
+    int p=numerator,q=denominator;
+    while(q != 0){
+      int a = p/q, r = p%q;
+      res.push_back(a);
+      p=q,q=r;
+    }
+    return res;
+  }
+  
+  std::vector<fraction> Construct_Fractional_Convergence_Sequence(){//分数収束列の構成
+    std::vector<int> C = this->continued_fraction();
+    //for(int i=0;i<C.size();i++) cerr<<C[i]<<"\n";
+    std::vector<fraction> res;
+    int pre_p = 1, pre_q = 0, p = C[0], q = 1;
+    res.push_back(fraction(p,q));
+    for(int i=1;i<C.size();i++){
+      int next_p = C[i]*p + pre_p;
+      int next_q = C[i]*q + pre_q;
+      pre_p=p,pre_q=q,p=next_p,q=next_q;
+      res.push_back(fraction(p,q));
+    }
+    return res;
+  }
+  
+  double val(){
+    return (double)numerator / (double)denominator;
+  }
+};
 
 fraction abs(fraction &x){return std::max(-x,x);}
 
@@ -237,7 +338,7 @@ std::vector<fraction> Calc_Hecke_operator(int p,int q,std::vector<int> &basis,st
 
 bool is_rectangular(std::vector<int> &basis,std::vector<std::vector<fraction>> &state,std::vector<fraction> &v_plus, std::vector<fraction> &v_minus,int p){
   //単位格子がrectangularかどうか返す
-  assert(state[0].size()>1);
+  assert(state[0].size() == 3);
   v_plus[1] = state[p-basis[0]][1]+1;
   v_plus[0] = -state[p-basis[1]][1];
   v_minus[1] = state[p-basis[0]][1]-1;
@@ -254,7 +355,7 @@ int main(){
     std::cout<<"p is not a prime :(\n";
     return 1;
   }
-  if(!is_Prime(l) || l%4!=3){
+  if(!is_Prime(l) || l%4!=3 || l%p == 0){
     std::cout<<"l is not valid :(\n";
     return 1;
   }
@@ -296,8 +397,11 @@ int main(){
   fraction m_minus = 0;
   for(int i=0;i<ssize(basis)-1;i++) m_minus += v_minus[i] * bases[i];
   std::cout<<"m_minus = "<<m_minus<<"\n";
-  
-  fraction result = L_f1_over_Omega_f.inv() * fraction(1,p) * Parametrization_Degrees[p] * m_minus.inv();
+  if(m_minus == 0){
+    std::cout<<"m_minus = 0";
+    return 0;
+  }
+  fraction result = L_f1_over_Omega_f.inv()/2 * fraction(1,p) * Parametrization_Degrees[p] * m_minus.inv();
   if(rectangular) result *= 2;
   else result *= 4;
   
